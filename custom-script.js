958217338ed40f2efe4d16b8685085b1a06d2b9c
Rhonnel Cordova <rhonneljickaincordova94@gmail.com>
2020-12-25 22:24:55 +0800

    Layout changes and content

--- custom-script.js
+++ custom-script.js
@@ -1,9 +1,5 @@
-
-	$(function(){
-		
-		$('#toggle-button').on('click', function() {
-		  $('.toggle-item').fadeToggle(1000);
-		  
-		});
-
-	});
+$(function () {
+  $("#toggle-button").on("click", function () {
+    $(".toggle-item").fadeToggle(1000);
+  });
+});
